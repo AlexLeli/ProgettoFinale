444bb74ee0f4a31ac025ae870345bb688b508dc2
AlexLeli <alexleli97@hotmail.com>
2025-06-07 21:04:05 +0200

    Add project files.

---
+++ Plugins/PluginsInMemory/InventoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using CoreBusiness;
+using UseCases.Interfaces;
+
+namespace PluginsInMemory
+{
+    public class InventoryRepository :IInventoryRepository
+    {
+        private List<Inventory> _inventories;
+
+        public InventoryRepository()
+        {
+            _inventories = new List<Inventory>()
+            {
+                new Inventory {}
+            };
+        }
+
+        public Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,26 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.Interfaces;
+
+namespace UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase
+    {
+        public readonly IInventoryRepository inventoryRepository;
+
+        
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository) 
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
