867efdd41c7cc32b81d4e815c23a06fe7e45505c
AlexLeli <alex.leli@meteda.it>
2025-06-16 15:46:50 +0200

    risolto bug

--- BlazorProgettoFinale/ViewModels/ViewModelsValidation/Produce_EnsureEnoughInventoryQuantity.cs
+++ BlazorProgettoFinale/ViewModels/ViewModelsValidation/Produce_EnsureEnoughInventoryQuantity.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using UseCases.Interfaces;
 
 namespace BlazorProgettoFinale.ViewModels.ViewModelsValidation
 {
@@ -7,13 +8,17 @@ namespace BlazorProgettoFinale.ViewModels.ViewModelsValidation
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
             var produceViewModel = validationContext.ObjectInstance as ProduceViewModel;
+            var repository = (IInventoryRepository?)validationContext.GetService(typeof(IInventoryRepository));
             if (produceViewModel != null)
             {
-                if (produceViewModel.Product != null)
+                
+                if (produceViewModel.Product != null && produceViewModel.Product.ProductInventory != null)
                 {
                     foreach (var productInventory in produceViewModel.Product.ProductInventory)
                     {
-                        if (productInventory != null && productInventory.InventoryQuantity * produceViewModel.QuantityToProduce > productInventory.Inventory.Quantity)
+                        var fullProduct =  repository.GetInventoryByIdAsync(productInventory.InventoryId).GetAwaiter().GetResult();
+
+                        if (productInventory != null && productInventory.InventoryQuantity * produceViewModel.QuantityToProduce > fullProduct.Quantity)
                         {
                             return new ValidationResult($"The inventory ({productInventory.Inventory.InventoryName} is not enough to produce {produceViewModel.QuantityToProduce} products)",
                                 new[] { validationContext.MemberName });
